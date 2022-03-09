f2ceb770cca5bc8a7a9fe0bb96043199f0f0abec
Matheson94 <mathesonchisholm@hotmail.com>
2022-03-09 20:36:00 +0000

    dashboard styled, functioning

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PW']
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
 end
