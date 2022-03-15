64e727ec02b41ca75ad9cec93f170b0d407054a3
Matheson94 <mathesonchisholm@hotmail.com>
2022-03-15 18:52:51 +0000

    product details spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "User clicks one product partial to see details", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+
+    end
+
+    scenario "They click a product to see details page" do
+      #ACT
+      visit root_path
+      find('a.btn-default').click
+      
+
+      #DEBUG
+      save_screenshot
+
+      #VERIFY
+      expect(page).to have_content('Description')
+
+    end
+end
